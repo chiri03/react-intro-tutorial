484caa0c1e731bea8282b0142b7efb899ab73c03
chiri03 <lena.kiriac.97@gmail.com>
2023-07-24 21:37:57 +0300

    end test projetc

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
+// // Import React and ReactDOM libraries
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
+// // Get a reference to the div with ID
 
-// ========================================
+// const el = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+// // Tell Rect to take control of that element
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+// const root = ReactDOM.createRoot(el);
+
+// // Create a component
+
+// function App() {
+//   return <div>
+//     <h1>Hi there!</h1>
+ 
+//   </div>;
+ 
+// }
+
+// // Show thw component on the screen
+
+// root.render(<App />);
+//1) Import the react and ReactDOM libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
+
+// 2) Get a refence to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell react to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Show the component on the screen
+root.render(<App />);
